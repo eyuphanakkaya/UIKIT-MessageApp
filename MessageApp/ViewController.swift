822fce81560cc31709375e73e74aa0d7d2795893
Eyuphan Akkaya <akkayaeyupp@gmail.com>
2023-09-05 15:17:54 +0300

    Initial Commit

---
+++ MessageApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by Eyüphan Akkaya on 5.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
